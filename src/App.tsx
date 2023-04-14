c77240cb8692fa828c1faffa9f8d768e9f584c7f
1Molot <sinkevich1990@gmail.com>
2023-04-14 21:23:11 +0300

    fix:movi-1yrock

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {v1} from "uuid";
+import {Watchlist} from "./components/Watchlist";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const title = 'Best movies'
+    const [movies, setMovies] = useState([
+        {id: v1(), name: 'The Shawshank Redemption', watched: false, rating: 93},
+        {id: v1(), name: 'The Godfather', watched: false, rating: 92},
+        {id: v1(), name: 'The Dark Knight', watched: false, rating: 91},
+        {id: v1(), name: 'The Godfather Part II', watched: false, rating: 90},
+        {id: v1(), name: 'Schindler\'s List', watched: false, rating: 89},
+        {id: v1(), name: 'The Lord of the Rings', watched: false, rating: 89},
+        {id: v1(), name: ' Pulp Fiction', watched: false, rating: 89}
+    ])
+
+    function removeFilms(id: string) {
+        setMovies(movies.filter(el => el.id !== id))
+    }
+
+    return (
+        <header className="App">
+            <div className={'main'}>
+                <ul>
+                    <Watchlist
+                        movies={movies}
+                        title={title}
+                        removeFilms={removeFilms}
+                    />
+                </ul>
+            </div>
+        </header>
+    );
 }
 
 export default App;
---
+++ src/components/Watchlist.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export type PropsType = {
+    movies: MovieType[]
+    title: string
+    removeFilms: (id: string) => void
+}
+
+export type MovieType = {
+    id: string
+    name: string
+    watched: boolean
+    rating: number
+}
+
+
+export const Watchlist = (props: PropsType) => {
+    return (
+        <>
+            <h3> {props.title} </h3>
+            <ul>
+                {props.movies.map((el) => {
+                    return (
+                        <li key={el.id}>
+                            <button onClick={() => {
+                                props.removeFilms(el.id)
+                            }}>x
+                            </button>
+                            <input type={'checkbox'} checked={el.watched}/>
+                            {`${el.name}: ${el.rating}`}
+                        </li>
+                    )
+                })}
+            </ul>
+        </>
+    );
+};
